ae65d5f77ee840faa6f2beafdb6386fa96ec9391
BarkaviSJ <barkavi15@gmail.com>
2020-08-18 16:05:56 +0100

    static file

--- STS/settings.py
+++ STS/settings.py
@@ -137,8 +137,5 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    ("admin", "/static/admin/css"), 
-]
 MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
